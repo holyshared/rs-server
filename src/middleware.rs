09588be8a516e169e012eef1d3f61bbe4da0ac6b
holyshared <holy.shared.design@gmail.com>
2016-02-25 14:39:49 +0900

    Return no content

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,11 +1,12 @@
 use nickel:: { Request, Response, Middleware, MiddlewareResult };
+use nickel::status::StatusCode;
 
 pub struct HealthCheckHandler;
 
 impl<D> Middleware<D> for HealthCheckHandler {
 
     fn invoke<'a>(&self, _: &mut Request<D>, res: Response<'a, D>) -> MiddlewareResult<'a, D> {
-        res.send("")
+        res.send(StatusCode::NoContent)
     }
 
 }
