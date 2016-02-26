069be45fcde4c3c75b1931c70c05862a89c4033a
holyshared <holy.shared.design@gmail.com>
2016-02-26 10:13:59 +0900

    add routing

--- src/routing.rs
+++ src/routing.rs
@@ -1,14 +1,12 @@
+use nickel:: { StaticFilesHandler };
 use nickel::router:: { Router, HttpRouter };
 use middleware:: { HealthCheckHandler };
+use std::env:: { current_dir };
 
 pub fn router() -> Router {
+    let root = current_dir().unwrap();
     let mut router = Router::new();
-    router.get("/foo", middleware! { |_|
-        "foo"
-    });
-    router.get("/bar", middleware! { |_|
-        "bar"
-    });
+    router.get("*", StaticFilesHandler::new(root.as_path()));
     router.get("/healthcheck", HealthCheckHandler);
     router
 }
