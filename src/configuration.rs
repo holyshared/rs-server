ab382e8e050104b95df2e2396a681938f5bd63f8
holyshared <holy.shared.design@gmail.com>
2016-02-26 13:32:44 +0900

    add toml file

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,49 +3,19 @@ use std::default:: { Default };
 use std::path:: { Path };
 use std::fs::File;
 use std::io:: { Read, Result };
-use std::io;
+//use std::io;
+use std::borrow:: { Borrow };
 
 pub struct Configuration {
     host: String,
     port: String
 }
-/*
+
 impl Configuration {
-    pub fn fromFile(path: Path) -> Self {
-
-
-
-        let mut f = try!(File::open("foo.txt"));
-        let mut s = String::new();
-        f.read_to_string(s);
-
-        match from_utf8(s.as_bytes()) {
-            Ok(v) => {
-                let mut parser = Parser::new(v);
-                match parser.parse() {
-                    Some(table) => {
-                        let host = table.get(&"host").unwrap();
-                        let port = table.get(&"port").unwrap();
-
-                        Configuration {
-                            host: host.to_string(),
-                            port: port.to_string()
-                        }
-                    },
-                    None => println!("parse errors: {:?}", parser.errors)
-                };
-            },
-            Err(err) => println!("{}", err)
-        };
-
-
-
-
-//        Configuration { host: "127.0.0.1".to_string(), port: "6767".to_string() }
+    pub fn fromFile(path: &Path) -> Self {
+        from_toml_file(path).unwrap()
     }
 }
-
-*/
 
 impl Default for Configuration {
     fn default() -> Self {
@@ -56,9 +26,29 @@ impl Default for Configuration {
     }
 }
 
-fn readTomlFile(path: &Path) -> Result<String> {
+pub fn from_toml_file(path: &Path) -> Option<Configuration> {
+    let content = read_file(path).unwrap();
+    let mut parser = Parser::new(content.borrow());
+
+    match parser.parse() {
+        Some(table) => {
+            let host = table.get(&"host".to_string()).unwrap();
+            let port = table.get(&"port".to_string()).unwrap();
+
+            Some(
+                Configuration {
+                    host: host.to_string(),
+                    port: port.to_string()
+                }
+            )
+        },
+        None => None
+    }
+}
+
+pub fn read_file(path: &Path) -> Result<String> {
     let mut file = try!(File::open(path));
     let mut buffer = String::new();
     let _ = try!(file.read_to_string(&mut buffer));
-    Ok(content.clone())
+    Ok(buffer.clone())
 }
